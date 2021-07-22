660036550a2a42524913ae8f64e1dbafe7eec35d
phi1ny3 <38825543+phi1ny3@users.noreply.github.com>
2021-07-21 23:29:40 -0600

    Update Sol3.py

--- Wk 11/Sol3.py
+++ Wk 11/Sol3.py
@@ -6,4 +6,4 @@ print(res)
 print(A)
 
 #2.
-print("index 1: 1, 33, 65 \nindex 2: 2 \n index6: 54 \n index8: 24 \nindex 9: 25, 57, 9 \nindex10: 42, 58 \nindex11: 75, 27 \nindex12: 12 \nindex13: 13 \nindex14: 46, 14")
+print("index 1: 1, 33, 65 \nindex 2: 2 \nindex6: 54 \nindex8: 24 \nindex 9: 25, 57, 9 \nindex10: 42, 58 \nindex11: 75, 27 \nindex12: 12 \nindex13: 13 \nindex14: 46, 14")
